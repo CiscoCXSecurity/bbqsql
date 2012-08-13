ecd0b09b23a93a7afd6687983903c945e35b9609
Mastahyeti <mastahyeti@gmail.com>
2012-08-12 21:14:06 -0500

    I think the setup.py is okay now

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-import bbqsql
+from setuptools import setup
 
-setup(name=bbqsql.__title__,
-      version=bbqsql.__version__,
-      license=bbqsql.__license__,
+setup(name='BBQSQL',
+      version='1.0',
+      license='BSD',
       author='Ben Toews (mastahyeti)',
       author_email='mastahyeti@gmail.com',
-      description='SQL Injcetion Exploitation Tool',
-      url='http://github.com/mastahyeti/bbqsql',
+      description='Rapid Blind SQL Injection Exploitation Tool',
+      url='http://github.com/neohapsis/bbqsql',
       packages=['bbqsql','bbqsql.lib','bbqsql.menu'],
       scripts=['scripts/bbqsql'],
-      requires=['gevent','requests','grequests']
+      install_requires=['gevent','requests','grequests']
      )
